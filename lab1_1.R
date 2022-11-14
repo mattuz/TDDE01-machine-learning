6a8ed7f99b04e6d30ca99fe97f92eb6ca18161b4
Simon ågren <seoagren@gmail.com>
2022-11-14 15:29:27 +0100

    ass 1 done, hopefully

--- lab1_1.R
+++ lab1_1.R
@@ -17,20 +17,22 @@ id3=setdiff(id1,id2)
 test=data[id3,]
 
 #Exercise 2
+install.packages('kknn')
 library(kknn)
-factor = as.factor()
+
 k_test <- kknn(as.factor(V65)~., train = train, test = test, k = 30, kernel = "rectangular")
 k_train <- kknn(as.factor(V65)~., train = train, test = train, k = 30, kernel = "rectangular")
 k_valid <- kknn(as.factor(V65)~., train = train, test = valid, k = 30, kernel = "rectangular")
 
-summary(k_nearest)
 table(k_test$fitted.values, as.factor(test$V65))
 train_table = table(k_train$fitted.values, train$V65)
 table(k_valid$fitted.values, valid$V65)
+
 missclass = function(X, X1) {
   n = length(X)
   return (1 - sum(diag(table(X1, X)))/n)
 }
+
 missclass(k_test$fitted.values, as.factor(test$V65))
 missclass(train$V65, k_train$fitted.values)
 missclass(k_valid$fitted.values, valid$V65)
@@ -51,7 +53,7 @@ for (i in 1:10){
   
   heatmap(my_heatmap, Colv = NA, Rowv = NA, main = i-1)
 }
-#Många är väldigt lika vet inte om vi ordnat rätt. 
+#M?nga ?r v?ldigt lika vet inte om vi ordnat r?tt. 
 
 
 #Exercise 4
@@ -60,7 +62,7 @@ val_miss_error <- numeric(30)
 
 
 for (i in 1:30){
-  k_test <- kknn(as.factor(V65)~., train = train, test = test, k = i, kernel = "rectangular")
+  k_valid <- kknn(as.factor(V65)~., train = train, test = valid, k = i, kernel = "rectangular")
   k_train <- kknn(as.factor(V65)~., train = train, test = train, k = i, kernel = "rectangular")
   
   train_miss_error[i] <- missclass(k_train$fitted.values, train$V65)
@@ -70,7 +72,8 @@ for (i in 1:30){
 
 plot(c(1:30), train_miss_error, ylab = "train_miss_error", xlab = "k", col="pink")
 points(c(1:30), val_miss_error, col="green")
-#Simon tänker att när de rosa cirklarna möter de gröna så ah rvi best k värde.
+
+#Simon t?nker att n?r de rosa cirklarna m?ter de gr?na s? ah rvi best k v?rde.
 
 k_test <- kknn(as.factor(V65)~., train = train, test = test, k = 9, kernel = "rectangular")
 k_train <- kknn(as.factor(V65)~., train = train, test = train, k = 9, kernel = "rectangular")
@@ -79,3 +82,49 @@ k_valid <- kknn(as.factor(V65)~., train = train, test = valid, k = 9, kernel = "
 missclass(k_test$fitted.values, as.factor(test$V65))
 missclass(train$V65, k_train$fitted.values)
 missclass(k_valid$fitted.values, valid$V65)
+
+#Exercise 5
+cross.entropy <- function(p, phat){
+  x <- 0
+  for (i in 1:length(p)){
+    x <- x + (p[i] * log(phat[i]))
+  }
+  return(-x)
+}
+
+train_miss_error <- as.vector(matrix(0,ncol = 30))
+val_miss_error <- as.vector(matrix(0,ncol = 30))
+
+entropy_error <- as.vector(matrix(0,ncol = 30))
+
+for (i in 1:30){
+  k_valid <- kknn(as.factor(V65)~., train = train, test = valid, k = i, kernel = "rectangular")
+  k_train <- kknn(as.factor(V65)~., train = train, test = train, k = i, kernel = "rectangular")
+  
+  
+  for (j in 0:9){
+    cross_val <- valid$V65 == j
+    cross_train <- train$V65 == j
+    
+    TRUE_val <- (which(cross_val, useNames = T))
+    prob_val <- k_valid$prob[cross_val, as.character(j)] + 1e-15
+    
+    TRUE_train <- (which(cross_train, useNames = T))
+    prob_train <- k_train$prob[cross_train, as.character(j)] + 1e-15
+    
+    
+    val_miss_error[i] <- cross.entropy(TRUE_val, prob_val)
+    train_miss_error[i] <- cross.entropy(TRUE_train, prob_train)
+    
+    
+    
+    entropy_error[i] <- abs(val_miss_error[i]-train_miss_error[i])
+  }
+  
+}
+
+print(entropy_error)
+plot(c(1:30), entropy_error, ylab = "cross_entropy_error", xlab = "k", col="blue")
+which.min(entropy_error)
+
+
